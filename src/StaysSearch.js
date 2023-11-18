9ae87c88a2cfc12992f552d62e5e18c288671b5f
Loganuwu <loganandrewbarajas@gmail.com>
2023-11-18 02:10:19 -0800

    api integration

--- src/StaysSearch.js
+++ src/StaysSearch.js
@@ -2,16 +2,40 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import axios from 'axios';
 
 const StaysSearch = ({ onSearch }) => {
     const [location, setLocation] = useState('');
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
+    const [hotels, setHotels] = useState([]);
+
+    const handleSearch = async () => {
+        const options = {
+            method: 'GET',
+            url: 'https://booking-com15.p.rapidapi.com/api/v1/hotels/searchDestination',
+            params: { query: location },
+            headers: {
+                'X-RapidAPI-Key': '8d36f60e47msha974aed1faa2b08p16ca05jsna91e6d65d953',
+                'X-RapidAPI-Host': 'booking-com15.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await axios.request(options);
+            setHotels(response.data); // Assuming response.data contains the array of hotels
+            // You might need to adjust this based on the actual structure of the response
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
         onSearch({ location, startDate, endDate });
     };
+
+
 
     return (
         <form onSubmit={handleSubmit} className="stay-search-form">
@@ -43,7 +67,18 @@ const StaysSearch = ({ onSearch }) => {
                 <label style={{ visibility: 'hidden' }}>Search:</label> {/* Invisible label for alignment */}
                 <button type="submit">Search Stays</button>
             </div>
+
+            <div className="hotel-list">
+                {hotels.map(hotel => (
+                    <div key={hotel.id} className="hotel-item">
+                        <h3>{hotel.name}</h3>
+                        {/* Add other hotel details you want to display */}
+                    </div>
+                ))}
+            </div>
         </form>
+
+        
     );
 };
 
