0749e69071d693f99c53e3ad77ce5996756ff0e0
Loganuwu <loganandrewbarajas@gmail.com>
2023-12-12 23:55:19 -0800

    wonky but working

--- src/StaysSearch.js
+++ src/StaysSearch.js
@@ -11,6 +11,9 @@ const StaysSearch = ({ onSearch }) => {
     const [startDate, setStartDate] = useState(null); // Set initial state to null
     const [endDate, setEndDate] = useState(null);     // Do the same for endDate if it's a range    
     const [hotels, setHotels] = useState([]);
+    const [searchInitiated, setSearchInitiated] = useState(false);
+    const [showResults, setShowResults] = useState(false); // State to control the visibility of the results container
+    const [hotelResults, setHotelResults] = useState([]); // State to store hotel results
 
     const formatDate = (date) => {
         return date.toISOString().split('T')[0];
@@ -58,15 +61,19 @@ const StaysSearch = ({ onSearch }) => {
                 'X-RapidAPI-Key': '8d36f60e47msha974aed1faa2b08p16ca05jsna91e6d65d953',
                 'X-RapidAPI-Host': 'booking-com15.p.rapidapi.com'
             }
+            
         };
 
         try {
             const response = await axios.request(searchDestinationOptions);
             const { latitude, longitude } = response.data.data[0]; // Get the first result's coordinates
             await searchHotelsByCoordinates(latitude, longitude);
+            setShowResults(true); // Show the results container after fetching data
         } catch (error) {
             console.error(error);
+            setShowResults(false); // Hide the results container on error
         }
+        setSearchInitiated(true);
     };
 
     const handleSubmit = (event) => {
@@ -93,7 +100,7 @@ const StaysSearch = ({ onSearch }) => {
 
     return (
         <>
-            <form onSubmit={handleSubmit} className="stay-search-form">
+            <form onSubmit={handleSubmit} className={`stay-search-form ${searchInitiated ? "top-aligned" : "centered"}`}>
                 <div className="input-column">
                         <label htmlFor="location">Location</label>
                         <div className="input-with-icon">
@@ -133,16 +140,18 @@ const StaysSearch = ({ onSearch }) => {
                 </div>
             </form>
 
-            <div className="hotel-list">
-                {hotels.slice(0, 10).map((hotel, index) => (
-                    <div key={index} className="hotel-item">
-                        <h3>{hotel.hotel_name}</h3>
-                        <img src={hotel.main_photo_url} alt={hotel.hotel_name} />
-                        {/* Add other hotel details you want to display */}
-                    </div>
-                ))}
-            </div>
+            {showResults && (
+                <div className="hotel-list">
+                    {hotels.slice(0, 10).map((hotel, index) => (
+                        <div key={index} className="hotel-item">
+                            <h3>{hotel.hotel_name}</h3>
+                            <img src={hotel.main_photo_url} alt={hotel.hotel_name} />
+                            {/* Add other hotel details you want to display */}
+                        </div>
+                    ))}
+                </div>
 
+            )}
         </>
     );
 };
