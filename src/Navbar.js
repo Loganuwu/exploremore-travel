c80c4472ef103c5791d5ed8dec32f9eba1a8062c
Loganuwu <loganandrewbarajas@gmail.com>
2024-01-10 03:28:33 -0800

    animated navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,19 +4,13 @@ import './Navbar.css';
 import { useHistory } from 'react-router-dom';
 
 const Navbar = () => {
-    const navigate = useNavigate();
-
-  const handleSupportClick = () => {
-    navigate('/support'); // Navigate to the support page using useNavigate
+    return (
+      <nav className="navbar">
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/support" className="nav-link">Support</Link>
+        {/* Add more nav links as needed */}
+      </nav>
+    );
   };
-
-  return (
-    <nav className="navbar">
-      <Link to="/">ExploreMore</Link>
-      <button onClick={handleSupportClick}>Support</button>
-      {/* Add other navigation items here */}
-    </nav>
-  );
-};
 
 export default Navbar; // Ensure this is at the end of the Navbar.js file
