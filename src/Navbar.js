25b2b3ad2599d218d7f87e9d4f0eeef5e2f576e7
Loganuwu <loganandrewbarajas@gmail.com>
2024-01-12 09:09:53 -0800

    navbar fixes

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom'; // useNavigate replaces useHistory
 import './Navbar.css';
 import ShoppingCartIcon from './images/shoppingcart.svg'; // Update the path as necessary
@@ -6,8 +6,32 @@ import OrderIcon from './images/order.svg'; // Update the path as necessary
 import { useHistory } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const hamburger = document.querySelector('.hamburger-menu');
+    const navMenu = document.querySelector('.nav-menu');
+
+    const toggleMenu = () => {
+      navMenu.classList.toggle('show');
+    };
+
+    hamburger.addEventListener('click', toggleMenu);
+
+    return () => {
+      hamburger.removeEventListener('click', toggleMenu);
+    };
+  }, []);
+  
+
     return (
       <nav className="navbar">
+      <div className="hamburger-menu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/exploremore-travel/support" className="nav-link">Support</Link>
         <Link to="/exploremore-travel/contact-us" className="nav-link">Contact Us</Link>
