2196b09f325ee85d37710c8245743b6a13e3e70b
Loganuwu <loganandrewbarajas@gmail.com>
2024-01-12 09:23:03 -0800

    fixed home pathing

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
       <Navbar />
       <CustomerServiceChat />
       <Routes>
-        <Route path="/exploremore-travel/" element={
+        <Route path="/exploremore-travel/home" element={
           <>
             <header className="app-header">
               <h1 className="website-title">ExploreMore</h1>
